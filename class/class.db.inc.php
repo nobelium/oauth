4169ae1c037dd981030d65462077922072b7906f
nobelium <vignesh_goldfish@yahoo.com>
2012-09-21 20:02:53 +0500

    i think db abstraction class is over

--- class/class.db.inc.php
+++ class/class.db.inc.php
@@ -9,10 +9,26 @@
  
 class db {
 	
-	protected $pdo;
+	private $pdo;
+	
+	private static $instance;
 	
 	function __construct($argument) {
-		
+		try{
+			$this->pdo = new PDO(DB_HOST.":host=".DB_HOST.";dbname=".DB_NAME,DB_USER,DB_PASSWD);
+			$this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$this->pdo->exec("set names 'utf8'");
+		}catch(PDOException $e){
+			die("Error <br/>Message: ".$e->getMessage());
+		}
+	}
+	
+	public static function instance(){
+		if(!isset(self::$instance)){
+			$classname = __CLASS__;
+			self::$instance = new $classname;
+		}
+		return self::$instance;
 	}
 	
 	public function exec($query){
@@ -29,6 +45,14 @@ class db {
 		}catch(PDOException $e){
 			die("Error in ".$query." <br/>Message: ".$e->getMessage()." <br/>Trace: ".$e->getTrace());
 		}
+	}
+	
+	public function lastinsertid(){
+		return $this->pdo->lastInsertId();
+	}
+	
+	public function quote($query){
+		return $this->pdo->quote($query);
 	}
 }
 
