64c2d25cbb4eafc92febca487a49428a696cbee6
nobelium <vignesh_goldfish@yahoo.com>
2012-09-21 19:09:18 +0500

    initial commit

---
+++ class/class.db.inc.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * This file contains class definition to abstract database interation that uses a pdo instance
+ * 
+ * Classname : db
+ * Filename  : class.db.inc.php
+ * 
+ * */
+ 
+class db {
+	
+	function __construct($argument) {
+		
+	}
+	
+	public function exec(){
+		
+	}
+	
+	public function query(){
+		
+	}
+}
+
+ 
+ 
+ 
+ 
+?>
---
+++ class/config.inc.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * This file contains database credentials
+ * 
+ * 
+ * 
+ * */
+ 
+define('DB_HOST', "localhost");
+define('DB_USER', "root");
+define('DB_PASSWD', "password");
+define('DB_NAME', "oauth");
+?>
