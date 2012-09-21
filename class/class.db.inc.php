7ec177558bd607ae0f7a1d083270766cd5082488
nobelium <vignesh_goldfish@yahoo.com>
2012-09-21 19:22:01 +0500

    building db abstraction layer

--- class/class.db.inc.php
+++ class/class.db.inc.php
@@ -9,16 +9,26 @@
  
 class db {
 	
+	protected $pdo;
+	
 	function __construct($argument) {
 		
 	}
 	
-	public function exec(){
-		
+	public function exec($query){
+		try{
+			return $this->pdo->exec($query);
+		}catch(PDOException $e){
+			die("Error in ".$query." <br/>Message: ".$e->getMessage()." <br/>Trace: ".$e->getTrace());
+		}
 	}
 	
-	public function query(){
-		
+	public function query($query){
+		try{
+			return $this->pdo->query($query);
+		}catch(PDOException $e){
+			die("Error in ".$query." <br/>Message: ".$e->getMessage()." <br/>Trace: ".$e->getTrace());
+		}
 	}
 }
 
--- class/config.inc.php
+++ class/config.inc.php
@@ -6,7 +6,8 @@
  * 
  * 
  * */
- 
+
+define('DB_TYPE', "mysql"); 
 define('DB_HOST', "localhost");
 define('DB_USER', "root");
 define('DB_PASSWD', "password");
